bb2d2d8a191d2ca00014241ce4d9e43d6de99ace
Greg Rynkowski <dev@rynkowski.info>
2017-02-22 11:17:45 +0000

    Add oh-my-zsh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -s `pwd`/home/.aliases ~/.aliases
 ln -s `pwd`/home/.bashrc ~/.bashrc
 ln -s `pwd`/home/.bash_profile ~/.bash_profile
 ln -s `pwd`/home/.gitconfig ~/.gitconfig
+ln -s `pwd`/home/.oh-my-zsh ~/.oh-my-zsh
 ln -s `pwd`/home/.zshrc ~/.zshrc
 
