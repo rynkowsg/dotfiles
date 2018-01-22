600a18bbcea49be05badafda6729fcec09cece4a
Greg Rynkowski <dev@rynkowski.info>
2018-01-22 17:49:09 +0000

    Correct ZSH config

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ln -s `pwd`/home/.aliases ~/.aliases
 ln -s `pwd`/home/.bashrc ~/.bashrc
 ln -s `pwd`/home/.bash_profile ~/.bash_profile
 ln -s `pwd`/home/.gitconfig ~/.gitconfig
-ln -s `pwd`/home/.oh-my-zsh ~/.oh-my-zsh
 ln -s `pwd`/home/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/home/.vimrc ~/.vimrc
 ln -s `pwd`/home/.zshrc ~/.zshrc
+ln -s `pwd`/refs/oh-my-zsh ~/.oh-my-zsh
