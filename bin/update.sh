827823d8725eb885fb5e02741c71bcac93c76b31
Greg Rynkowski <>
2021-05-26 21:27:17 +0100

    Add update.sh script

---
+++ bin/update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# update repo
+git fetch -p origin
+git rebase -i origin/master
+git submodule update --init --recursive
+
+# install all the links again
+SCRIPT_DIR="$(cd "$(dirname "$([ -L "$0" ] && readlink "$0" || echo "$0")")" || exit 1; pwd -P)"
+ROOT_DIR="$(cd "${SCRIPT_DIR}/.." || exit 1; pwd -P)"
+bash "${ROOT_DIR}/bin/install.sh"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # update packages
+  brew update
+  brew upgrade
+fi
+
+# Remind to update oh-my-zsh
+GREEN="\e[0;92m"; RESET="\e[0m" # https://techstop.github.io/bash-script-colors/
+printf "${GREEN}%s\n" "To update oh-my-zsh call:"
+printf "%s${RESET}\n" "  omz update"
