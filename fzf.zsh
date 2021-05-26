aef64aa921f59c273c27435ebb2b898f2cc914c7
Greg Rynkowski <>
2021-05-26 21:14:43 +0100

    Make FZF integration platform agnostic

---
+++ fzf.zsh
@@ -0,0 +1,19 @@
+#######################################################################
+# These are files geberated by /usr/local/opt/fzf/install.            #
+# I replaced /usr/local/opt/fzf by $FZF_ROOT to be platform agnostic. #
+# FZF_ROOT is set in platformrc.                                      #
+# #####################################################################
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == *${FZF_ROOT}/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${FZF_ROOT}/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "${FZF_ROOT}/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "${FZF_ROOT}/shell/key-bindings.zsh"
