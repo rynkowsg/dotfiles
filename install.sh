83e18b146c1a8720958dd1a19e9ecedead066701
Greg Rynkowski <>
2021-05-26 18:13:09 +0100

    Correct launching dotfiles ./install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #set -x
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" || exit 1; pwd -P)"
-DOTFILES_DIR="${SCRIPT_DIR}"
+SCRIPT_DIR="$(cd "$(dirname "$([ -L "$0" ] && readlink "$0" || echo "$0")")" || exit 1; pwd -P)"
+ROOT_DIR="${SCRIPT_DIR}"
 
-TMP_DIR=$(mktemp -d -t "dotfiles-$(date +%Y%m%d_%H%M%S)-")
+# install dependencies
+source "${ROOT_DIR}/lib/install_dotfiles_deps" && install_dotfiles_deps "${ROOT_DIR}"
+
+# link all the files
+
+TMP_DIR=$(mktemp -d -t "dotfiles-$(date +%Y%m%d_%H%M%S)-XXXXXXXX")
 INSTALL_SCRIPT="${TMP_DIR}/install.sh"
 
 # rcup generates a shell script installing all the links
-env RCRC="${DOTFILES_DIR}/rcrc" rcup -B 0 -g > "${INSTALL_SCRIPT}"
+env RCRC="${ROOT_DIR}/rcrc" rcup -B 0 -g > "${INSTALL_SCRIPT}"
 
 printf "%s\n" "---------------------------------------------------------------"
 printf "%s\n" "---               GENERATED SCRIPT                         ----"
