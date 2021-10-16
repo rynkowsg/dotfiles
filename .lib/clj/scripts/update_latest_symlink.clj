49cfb560de9f6a0528de064c346371bc7dfcea2e
Greg Rynkowski <greg@rynkowski.pl>
2021-10-17 00:32:23 +0100

    Add updating latest symlinks in Android SDK

---
+++ .lib/clj/scripts/update_latest_symlink.clj
@@ -0,0 +1,40 @@
+#!/usr/bin/env bb
+
+(require '[babashka.deps :as deps])
+(deps/add-deps '{:deps {babashka/fs    {:mvn/version "0.1.0"}
+                        clj-commons/fs {:mvn/version "1.6.309"}}})
+
+(ns update-latest-symlink
+  (:require
+   [clojure.string :as str]
+   [me.raynes.fs :as fs]))
+
+(defn pick-latest [dir]
+  (->> (fs/list-dir dir)
+       (filter #(not (str/ends-with? % "latest")))
+       (sort)
+       (last)))
+
+(comment
+ (fs/delete symlink-path)
+ (fs/sym-link symlink-path latest-build-tools)
+ (pick-latest "/usr/local/opt/android-sdk/build-tools"))
+
+(defn create-symlink [path target]
+  (fs/sym-link path target)
+  (println "[update-symlinks]   added" (str path) " => " (str target)))
+
+(defn create-latest-in-dir [dir]
+  (let [latest-one      (str (pick-latest dir))
+        symlink-path    (fs/file dir "latest")
+        exists          (fs/link? symlink-path)
+        previous-target (when exists (str (fs/read-sym-link symlink-path)))]
+    (when (not exists)
+      (create-symlink symlink-path latest-one))
+    (when (and exists (not= previous-target latest-one))
+      (fs/delete symlink-path)
+      (println "[update-symlinks] removed" (str symlink-path) " => " (str previous-target))
+      (create-symlink symlink-path latest-one))))
+
+(let [dirs *command-line-args*]
+  (run! create-latest-in-dir dirs))
