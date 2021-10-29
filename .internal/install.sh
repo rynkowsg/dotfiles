2872dd518a2d6c72f584da5f5e80f4bc39895929
Greg Rynkowski <greg@rynkowski.pl>
2021-10-29 19:31:22 +0100

    Adjust setup for storing passwords with pass

--- .internal/install.sh
+++ .internal/install.sh
@@ -27,3 +27,8 @@ yes | env VERBOSE=1 sh "${INSTALL_SCRIPT}"
 rm -rf "${TMP_DIR}"
 
 bash "${ROOT_DIR}/.internal/inject_secrets.sh"
+
+# checkout password stores
+mkdir -p ~/.password-store
+git clone git@github.com:rynkowsg/rynkowski-pass-store.git ~/.password-store/rynkowski
+git clone git@github.com:rynkowsg/chargedup-pass-store.git ~/.password-store/chargedup
