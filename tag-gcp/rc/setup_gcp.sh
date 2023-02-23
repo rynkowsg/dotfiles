7606a77684d389796258864fd10aaed9285b9f57
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-02-23 17:05:58 +0000

    Extract setups from commonrc to tags

---
+++ tag-gcp/rc/setup_gcp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# debug log
+[ "${RC_DEBUG}" = true ] && printf "reading: %s\t\t($0)\n" "setup_gcp"
+
+# Setup   # TODO: Correct for Linux
+GOOGLE_CLOUD_HOME="$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+# shellcheck disable=SC2039
+if [ -n "$BASH_VERSION" ]; then
+  source "${GOOGLE_CLOUD_HOME}/path.bash.inc"
+  source "${GOOGLE_CLOUD_HOME}/completion.bash.inc"
+elif [ -n "$ZSH_VERSION" ]; then
+  source "${GOOGLE_CLOUD_HOME}/path.zsh.inc"
+  source "${GOOGLE_CLOUD_HOME}/completion.zsh.inc"
+elif [ -n "$FISH_VERSION" ]; then
+  source "${GOOGLE_CLOUD_HOME}/path.fish.inc"
+fi
