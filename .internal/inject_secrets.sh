ceb06e2168552371ac8d9518ec06408102a4eef7
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-02-24 01:44:47 +0000

    Add filling templates with non-secret values

--- .internal/inject_secrets.sh
+++ .internal/inject_secrets.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# https://docs.datomic.com/cloud/dev-local.html#getting-started
-sed -i "s|?HOME?|${HOME}|g" "${HOME}/.datomic/dev-local.edn"
-
 COGNITECT_SERVER_ID="?cognitect-server-id?"
 COGNITECT_USER="?cognitect-username?"
 COGNITECT_PASS="?cognitect-password?"
