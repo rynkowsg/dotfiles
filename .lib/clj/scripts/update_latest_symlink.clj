dacd63aa8519da74df50f96b36be87f93ff44c95
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2025-05-14 17:04:55 +0200

    feat(android): Update symlinks automatically

--- .lib/clj/scripts/update_latest_symlink.clj
+++ .lib/clj/scripts/update_latest_symlink.clj
@@ -1,7 +1,7 @@
 #!/usr/bin/env bb
 
-#?(:bb      (do (require '[babashka.deps])
-                (babashka.deps/add-deps '{:deps {clj-commons/fs {:mvn/version "1.6.309"}}}))
+#?(:bb (do (require '[babashka.deps])
+           (babashka.deps/add-deps '{:deps {clj-commons/fs {:mvn/version "1.6.309"}}}))
    :default (do))
 
 (ns update-latest-symlink
@@ -19,25 +19,24 @@
 (comment
  (bfs/create-sym-link "/tmp/latest" "/tmp/sample-target-file")
  (bfs/delete "/tmp/latest")
- (pick-latest "/usr/local/opt/android-sdk/build-tools"))
+ (pick-latest "/home/user/.android/sdk/build-tools"))
 
 (defn create-symlink [path target]
   (bfs/create-sym-link path target)
   (println "[update-symlinks]   added" (str path) " => " (str target)))
 
-(defn create-latest-in-dir [dir]
-  (if (fs/exists? dir)
-    (let [latest-one      (str (pick-latest dir))
-          symlink-path    (bfs/file dir "latest")
-          exists          (bfs/sym-link? symlink-path)
+(defn create-latest [dir symlink-path]
+  (if (not (fs/exists? dir))
+    (println "[update-symlinks] directory doesn't exist:" dir)
+    (let [latest-one (str (pick-latest dir))
+          exists (bfs/sym-link? symlink-path)
           previous-target (when exists (str (fs/read-sym-link symlink-path)))]
       (when (not exists)
         (create-symlink symlink-path latest-one))
       (when (and exists (not= previous-target latest-one))
         (bfs/delete symlink-path)
-        (println "[update-symlinks] removed" (str symlink-path) " => " (str previous-target))
-        (create-symlink symlink-path latest-one)))
-    (println "[update-symlinks] directory doesn't exist:" dir)))
+        (println "[update-symlinks] reset" (str symlink-path) " => " (str previous-target))
+        (create-symlink symlink-path latest-one)))))
 
-(let [dirs *command-line-args*]
-  (run! create-latest-in-dir dirs))
+(let [[dir path] *command-line-args*]
+  (create-latest dir path))
